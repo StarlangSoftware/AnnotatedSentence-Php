162f91833a9d255e9d14309bda2bc467a57a83b7
olcaytaner <olcay.yildiz@ozyegin.edu.tr>
2025-10-11 11:47:39 +0300

    Added integer values to enum type ViewLayerType

--- src/ViewLayerType.php
+++ src/ViewLayerType.php
@@ -2,26 +2,27 @@
 
 namespace olcaytaner\AnnotatedSentence;
 
-enum ViewLayerType
+enum ViewLayerType: int
 {
-    case PART_OF_SPEECH;
-    case INFLECTIONAL_GROUP;
-    case META_MORPHEME;
-    case META_MORPHEME_MOVED;
-    case TURKISH_WORD;
-    case PERSIAN_WORD;
-    case ENGLISH_WORD;
-    case WORD;
-    case SEMANTICS;
-    case NER;
-    case DEPENDENCY;
-    case PROPBANK;
-    case SHALLOW_PARSE;
-    case ENGLISH_PROPBANK;
-    case ENGLISH_SEMANTICS;
-    case FRAMENET;
-    case SLOT;
-    case POLARITY;
-    case CCG;
-    case POS_TAG;
+    case PART_OF_SPEECH = 0;
+    case INFLECTIONAL_GROUP = 1;
+    case META_MORPHEME = 2;
+    case META_MORPHEME_MOVED = 3;
+    case TURKISH_WORD = 4;
+    case PERSIAN_WORD = 5;
+    case ENGLISH_WORD = 6;
+    case WORD = 7;
+    case SEMANTICS = 8;
+    case NER = 9;
+    case DEPENDENCY = 10;
+    case PROPBANK = 11;
+    case SHALLOW_PARSE = 12;
+    case ENGLISH_PROPBANK = 13;
+    case ENGLISH_SEMANTICS = 14;
+
+    case FRAMENET = 15;
+    case SLOT = 16;
+    case POLARITY = 17;
+    case CCG = 18;
+    case POS_TAG = 19;
 }
